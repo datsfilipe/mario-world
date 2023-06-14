29198ebcd18650d406a678d92062766acd57b21d
datsfilipe <datsfilipe.foss@proton.me>
2023-06-14 03:45:49 -0300

    feat(toad-heart): add posts routes into main routes

--- packages/server/toad-heart/src/infra/http/routes.ts
+++ packages/server/toad-heart/src/infra/http/routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 
 import userRouter from "@modules/users/infra/http";
-// import { postsRouter } from "@modules/posts/infra/http";
+import postRouter from "@modules/posts/infra/http";
 
 const router = Router();
 
 router.use("/user", userRouter);
-// router.use("/posts", postsRouter);
+router.use("/posts", postRouter);
 
 export { router };
