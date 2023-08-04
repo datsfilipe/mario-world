af532566510c8647e8b398ac3387cfdc1ae5a3ad
datsfilipe <datsfilipe.foss@proton.me>
2023-08-04 12:40:52 -0300

    refactor(yoshi-dispatch): change url to customer

--- packages/server/yoshi-dispatch/src/modules/customer/infra/http/index.ts
+++ packages/server/yoshi-dispatch/src/modules/customer/infra/http/index.ts
@@ -3,7 +3,7 @@ import type { RouteOptions } from "fastify";
 export default [
   {
     method: "GET",
-    url: "/",
+    url: "/customer",
     handler: async () => {
       return { hello: "world" };
     },
